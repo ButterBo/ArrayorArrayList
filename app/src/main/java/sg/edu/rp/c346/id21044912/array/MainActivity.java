a72c7866cfa9532cf84674eddd78c1acab046fad
ButtorBo <21044912@myrp.edu.sg>
2022-06-08 11:55:09 +0800

    ArrayList conversion

--- app/src/main/java/sg/edu/rp/c346/id21044912/array/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21044912/array/MainActivity.java
@@ -4,33 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
-    String[] fruits;
+    ArrayList<String> fruits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle("ArrayList");
+
+        fruits = new ArrayList<String>();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("cherry");
+        fruits.add("durian");
 
         tv = findViewById(R.id.textView);
 
-        fruits = new String[3];
-        fruits[0] = "apple";
-        fruits[1] = "banana";
-        fruits[2] = "cherry";
+        String text = "Fruits\n";
+        text += "=====\n";
 
-        tv = findViewById(R.id.textView);
-
-        StringBuilder text = new StringBuilder("Fruits\n");
-        text.append("=====\n");
-
-        for (String fruit : fruits) {
-            text.append(fruit).append("\n");
+        for (int i=0; i<fruits.size();i++) {
+            text += fruits.get(i) + "\n";
         }
 
-        tv.setText(text.toString());
+        tv.setText(text);
     }
 }
